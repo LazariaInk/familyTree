5feaeb7c97467fb7f0d74b3b9ea2103ce428d1f1
LazariaInk <67344017+LazariaInk@users.noreply.github.com>
2021-09-24 00:45:46 +0300

    Add pages in angular, made services for branches

--- front end/familyTree/src/app/app.component.ts
+++ front end/familyTree/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Branch} from "./shared/models/branch";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'familyTree';
+  public branch : Branch[];
 }
